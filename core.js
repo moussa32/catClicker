896a99b3501a2bf5d5becb2a6b6e9fac3a73036d
Moussa Ibrahim <moussaebrahem32@gmail.com>
2020-03-21 21:07:19 +0200

    Premium version

--- core.js
+++ core.js
@@ -3,37 +3,45 @@ const displayedCatName = document.querySelector('#displayedCatName');
 const changeCatButton = document.querySelector('#changeCatButton');
 const clickedNum = document.querySelector('#clickedNum');
 
-let cats = {
-    cat1:{
+let cats = [
+    {
         id:0,
         name:'Napalm',
         pictureSRC:'images/cat1.jpg',
         clickNumber:0
     },
-    cat2:{
+    {
         id:1,
         name:'McBurpyTuna',
         pictureSRC:'images/cat2.jpg',
         clickNumber:0
+    },{
+        id:2,
+        name:'Oliver',
+        pictureSRC:'images/cat3.jpg',
+        clickNumber:0
+    },{
+        id:3,
+        name:'Leo',
+        pictureSRC:'images/cat4.jpg',
+        clickNumber:0
+    },{
+        id:4,
+        name:'Max',
+        pictureSRC:'images/cat5.jpg',
+        clickNumber:0
     }
+]
+
+let currentCat = 0;
+
+function init(){
+    praimeryCatImage.setAttribute('src',cats[currentCat].pictureSRC);
+    displayedCatName.innerHTML = cats[currentCat].name;
+    clickedNum.innerHTML = cats[currentCat].clickNumber;
 }
 
-window.onload = function(){
-    praimeryCatImage.src = cats.cat1.pictureSRC;
-    displayedCatName.innerHTML = cats.cat1.name;
-    clickedNum.innerHTML = cats.cat1.clickNumber;
-}
-function toggleCats(){
-    if(displayedCatName.innerHTML == cats.cat1.name){
-        praimeryCatImage.src = cats.cat2.pictureSRC;
-        displayedCatName.innerHTML = cats.cat2.name;
-        clickedNum.innerHTML = cats.cat2.clickNumber;
-    }else{
-        praimeryCatImage.src = cats.cat1.pictureSRC;
-        displayedCatName.innerHTML = cats.cat1.name;
-        clickedNum.innerHTML = cats.cat1.clickNumber;
-    }
-}
+window.onload = init();
 
 praimeryCatImage.addEventListener('click',() =>{
     parseInt(clickedNum.innerHTML,10);
@@ -41,4 +49,14 @@ praimeryCatImage.addEventListener('click',() =>{
     clickedNum.textContent = clickedNum.innerHTML;
 });
 
-changeCatButton.addEventListener('click',toggleCats);
+changeCatButton.addEventListener('click',() =>{
+    if(currentCat < cats.length){
+        currentCat += 1;
+        console.log(currentCat);
+        if(currentCat == cats.length){
+            currentCat = 0;
+            init();
+        }
+        init();
+    }
+});
